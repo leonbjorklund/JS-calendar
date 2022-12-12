2062d35ada261ad2031c58fadf6db4b5e822e153
lysmac <calle@lysmask.se>
2022-12-12 14:02:37 +0100

    fixed some styling for the output

--- public/js/today.js
+++ public/js/today.js
@@ -1,20 +1,26 @@
 function createTimeDateMonthMsg() {
   const date = new Date().toLocaleDateString("en-SE", {
     day: "numeric",
+    month: "short",
+  });
+
+  const englishDayName = new Date().toLocaleDateString("en-SE", {
     weekday: "long",
-    month: "long",
   });
 
   const time = new Date().toLocaleTimeString("sv-SE", {
     hour: "numeric",
     minute: "numeric",
-    second: "numeric",
   });
 
   const today = new Date();
   const year = today.getFullYear();
 
+  document.getElementById("displayCurrentTime").textContent = time;
+
   document.getElementById(
-    "timeDayDate"
-  ).textContent = `The time is: ${time} and the date is ${date} ${year}`;
+    "displayTodaysDay"
+  ).textContent = `Today ${englishDayName}`;
+
+  document.getElementById("displayTodaysDateMonth").textContent = ` ${date}`;
 }
