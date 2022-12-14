ee6a12a409562bd3f1069b578f64aa404476d1f3
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2022-12-14 16:49:52 +0100

    Created js file and wrote plan.

---
+++ public/js/calendar-todos.js
@@ -0,0 +1,13 @@
+// Get todos from local storage
+// Get all from current month
+
+// Store to some kind of variable
+
+// Check variable for a todo from the current date during day square creation
+
+// Show number of todos found
+
+// Should update when a todo is added.
+// Todos will have to be mapped to the calendar separately from it's rendering.
+// Todos rendering can then be called again with each addition or removal of a todo.
+// Perhaps only call the function if the addition or removal happens on the current openMonth to avoid unnecessary work.
