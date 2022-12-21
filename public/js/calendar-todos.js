c8782665102f769bf9f68d0493551b8f06d3d1dd
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2022-12-21 16:24:41 +0100

    Fixed yearly repetition bug.

--- public/js/calendar-todos.js
+++ public/js/calendar-todos.js
@@ -24,7 +24,8 @@ function filterMonthTodos(allLocalTodos) {
     const openMonthTodos = [];
     for (const todo of allLocalTodos) {
         let splitDate = todo.date.split('-');
-        if (parseInt(splitDate[1]) === openMonth.monthNr + 1) {
+        console.log(splitDate[0], openMonth.year)
+        if (parseInt(splitDate[1]) === openMonth.monthNr + 1 && parseInt(splitDate[0]) === openMonth.year) {
             openMonthTodos.push(todo);
         }
     }
